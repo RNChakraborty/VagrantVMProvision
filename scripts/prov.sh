de48e4679d78b79c6ba6d10bf94642c6288a59a8
RNChakraborty <rudraneel.chakraborty@gmail.com>
2017-07-20 21:16:47 -0400

    V3

--- scripts/prov.sh
+++ scripts/prov.sh
@@ -19,8 +19,72 @@ if [ -z "$githb" ]; then
   touch gitcreate
 fi
 }
+apache_prov(){
+apache=$( command -v apache2 );
+if [ -z "$apache" ]; then
+  sudo apt-get install -y apache2 >/dev/null 2>&1
+  echo "Finished Installing Apache!!!"
+  touch apachecreate
+fi
+
+}
+
+check_installation(){
+ msql=$( command -v mysql );
+ apache=$( command -v apache2 );
+ githb=$( command -v git );
+ msql_print=''
+ apache_print=''
+ github_print=''
+ all_installation_complete='true'
+ 
+ if [ -z "$githb" ]; then
+     github_print='Not Installed!'
+	 all_installation_complete='false'
+ else
+    github_print='Installed'
+ fi
+ 
+ if [ -z "$msql" ]; then
+     msql_print='Not Installed!'
+	 all_installation_complete='false'
+ else
+    msql_print='Installed'
+ fi
+ 
+if [ -z "$apache" ]; then
+     apache_print='Not Installed!'
+	 all_installation_complete='false'
+ else
+    apache_print='Installed'
+ fi
+  echo "Github..."$github_print
+  echo "MySql..."$msql_print
+  echo "Apache..."$apache_print
+ 
+ if [[ "$all_installation_complete" = "true" ]]; then
+     echo "All installation completed !!! Preparing to clone a sample git repository"
+ else
+    echo "Problem with installation. Aborting !!!!"
+ fi
+}
+
+prepare_directory_and_clone_git(){
+sudo addgroup pythian >/dev/null 2>&1
+sudo useradd pythian -g pythian
+echo pythian:pythian123 | chpasswd
+sudo mkdir /opt/code/
+sudo git clone git://github.com/RNChakraborty/SystemINFO.git /opt/code/ >/dev/null 2>&1
+sudo chown -R pythian:pythian /opt/code/
+sudo chmod 750 /opt/code/
+}
+
+
 
 echo "****Started Provisioning*****\n"
 sudo apt-get update >/dev/null 2>&1
 mysql_prov
 github_prov
+apache_prov
+check_installation
+prepare_directory_and_clone_git
